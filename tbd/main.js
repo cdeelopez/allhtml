f661cebd09a6cf9a86c961bf52ad6cd793431616
cdeelopez <cdeelopez@gmail.com>
2022-03-05 21:08:38 +0800

    Update main.js

--- tbd/main.js
+++ tbd/main.js
@@ -2,7 +2,6 @@ const setupGame = (guessme) => {
     let html = '<div>'
     let isSpace = false
     for (const chr of guessme) {
-        //const c = chr === " " ? "space" : "letter"
         if (chr === " ") { 
             isSpace = true 
             html += '</div><div>'
@@ -18,30 +17,44 @@ const setupGame = (guessme) => {
 }
 $(function() {
     // todo get one per day
-    const guessme = phrases[0].toUpperCase()
+    const guessme = phrases[1].toUpperCase()
     const guessmeLetters = guessme.replaceAll(' ', '')
     const lettersCnt = guessmeLetters.length
     let counter = 0
     let showInterval
     let displayed = 0
-    
-    setupGame(guessme)
 
-    $(".guess-now").on("click", () => {
-        onGuess()
+    const endGame = () => {
         clearInterval(showInterval)
         showInterval = undefined
-    })
+        displayLostPopup()
+        
+        setStats(items.LOSSES, grades.F)
+    }
 
-    $(".main-section").on("click", ".submit", () => {
-        const yes = onGuessSubmit(guessmeLetters)
-        $(".guess-popup").hide()
-        if (yes) winGame()
-        else endGame()
-    })
+    const displayLostPopup = () => {
+        $(".error-popup h2").addClass(grades.F)
+        $(".error-popup p").text(guessme)
+        $(".error-popup").show()
+    }
 
-    const showLetter = () => {
+    const winGame = () => {
+        const pct = (displayed / lettersCnt) * 100
+        const grade = getGrade(pct)
+        displayWonPopup(grade)
+        const currGradeCount = localStorage.getItem(grade) || 0
+        localStorage.setItem(grade, parseInt(currGradeCount) + 1)
+        setStats(items.WINS, grade)
+    }
+
+    const displayWonPopup = (grade) => {
+        $(".win-popup h2").addClass(grade)
+        $(".win-popup").show()
+    }
+
+    const showLetter = (isSettingUp) => {
         const letter = pattern[counter]
+        if(!isSettingUp) setGameState(counter)
         if (counter >= pattern.length) {
             endGame()
             return
@@ -63,41 +76,91 @@ $(function() {
         }
         else {
             counter++
-            showLetter()
+            showLetter(isSettingUp)
+        }
+    }
+    
+    setupGame(guessme)
+    const todaysState = getTodaysStat()
+    if(todaysState) {
+        while(counter <= todaysState.counter)
+            showLetter(true)
+        if(todaysState.isComplete) {
+            if(todaysState.didWin) displayWonPopup(todaysState.grade)
+            else displayLostPopup(grades.F)
         }
     }
 
-    const endGame = () => {
-        clearInterval(showInterval)
-        showInterval = undefined
-        $(".error-popup p").text(guessme)
-        $(".error-popup").show()
-    }
-
-    const winGame = () => {
-        const pct = (displayed / lettersCnt) * 100
-        $(".win-popup p").text(`${pct.toFixed(2)}%`)
-        $(".win-popup").show()
-    }
-        
     if (typeof showInterval === 'undefined'){
-        showLetter()
-        counter++
-        showInterval = setInterval(showLetter, 5000)
+        if(!todaysState || (todaysState && !todaysState.isComplete))
+            showInterval = setInterval(showLetter, 3000)
     } else {
         clearInterval(showInterval)
     }
+
+    $(".guess-now").on("click", () => {
+        onGuess()
+        clearInterval(showInterval)
+        showInterval = undefined
+    })
+
+    $(".main-section").on("click", ".submit", () => {
+        const yes = onGuessSubmit(guessmeLetters)
+        $(".guess-popup").hide()
+        if (yes) winGame()
+        else endGame()
+    })
+
+    $(".main-section").on("keyup", "input[type='text']", (e) => {
+        const inputs = $(e.target).closest(".guessbox").find("input[type='text']")
+        if(e.keyCode == 8) {
+            inputs.eq(Math.max(inputs.index(e.target) - 1, 0))
+                .val("")
+                .focus()
+
+        } else {
+            inputs.eq(inputs.index(e.target) + 1).focus();
+        }
+    })
 
     // const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     // console.log(randomizeItems(alphabet))
 
 });
 
+const setGameState = (counter) => {
+    localStorage.setItem(items.GAME_STATE, `{ "counter": ${counter} }` )
+}
+
+const getTodaysStat = () => {
+    const lastPlayed = localStorage.getItem(items.LAST_PLAYED)
+    const todaysDt = getTodaysDt()
+    if(lastPlayed != todaysDt) {
+        localStorage.setItem(items.LAST_PLAYED,todaysDt)
+        localStorage.removeItem(items.GAME_STATE)
+    }
+
+    return getGameState()
+}
+
+const getGameState = () => {
+    const state = localStorage.getItem(items.GAME_STATE)
+    if(!state) return state
+    return $.parseJSON(state)
+}
+
+const setupWithTodaysState = () => {
+    const state = localStorage.getItem(items.GAME_STATE)
+    if(!state) return
+    const gameState = $.parseJSON(state)
+    console.log(gameState)
+}
+
 const onGuess = () => {
     const guessContent = $(".guessbox").clone()
     $(guessContent).find("span").each(function() {
         if(!$(this).text().trim().length) {
-            $(this).html("<input type='text'>")
+            $(this).html("<input type='text' maxlength='1'>")
         }
     })
     const guessContentHtml = $(guessContent).html()
@@ -126,6 +189,28 @@ const onGuessSubmit = (answer) => {
     return false
 }
 
+const setStats = (stat, grade) => {
+    const cnt = localStorage.getItem(stat) || 0
+    localStorage.setItem(stat, parseInt(cnt) + 1)
+    const currStats = getGameState()
+    currStats.didWin = stat === items.WINS
+    currStats.isComplete = true
+    currStats.grade = grade
+    localStorage.setItem(items.GAME_STATE, JSON.stringify(currStats))
+}
+
+const getGrade = (pct) => {
+    if(pct >= 0 && pct <= 40.0) return grades.A
+    else if(pct > 40.0 && pct <= 60.0) return grades.B
+    else if(pct > 60.0 && pct <= 80.0) return grades.C
+    else return grades.D
+}
+
+const getTodaysDt = () => {
+    const dt = new Date()
+    return `${dt.getMonth()+1}/${dt.getDate()}/${dt.getFullYear()}`
+}
+
 const randomizeItems = (items) => items
         .map((a) => ({sort: Math.random(), value: a}))
         .sort((a, b) => a.sort - b.sort)
@@ -140,3 +225,18 @@ const phrases = [
     "A mile a minute",
     "Based on actual events"
 ]
+
+const items = {
+    WINS: "res-wins",
+    LOSSES: "res-losses",
+    LAST_PLAYED: "last-played-date",
+    GAME_STATE: "game-state",
+}
+
+const grades = {
+    A: "grade-a",
+    B: "grade-b",
+    C: "grade-c",
+    D: "grade-d",
+    F: "grade-f",
+}
